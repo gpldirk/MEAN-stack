e58bb132042b9e03074d16ddf9f7e2505c0f3f18
gpldirk <gepeilu@usc.edu>
2020-07-28 11:28:08 -0700

    first commit

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -3,6 +3,7 @@ const ExtractJwt = require('passport-jwt').ExtractJwt
 const User = require('../models/user')
 const config = require('../config/database')
 
+// jwt auth using passport
 module.exports = function(passport) { 
   let opts = {}
   opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt")
